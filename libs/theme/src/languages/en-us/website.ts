c9da41c33cfd75af17a4b16c65ff2bd5c575fdcd
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-11-20 02:03:59 +0100

    added translations for authentication page

--- libs/theme/src/languages/en-us/website.ts
+++ libs/theme/src/languages/en-us/website.ts
@@ -1,3 +1,12 @@
 export const website = {
-  hello:'Hello'
-  };
+  useDeveloperAccount: 'Use a Developer Account',
+  signInWithGoogle: 'Sign in with Google',
+  authenticateWithGoogle: 'Authenticate with Google',
+  developerSignInCaption:
+    'Authenticate with google below to become or access your developer account',
+  signIn: 'Sign In',
+  signInCaption: 'Sign in with google to gain access to your workspace',
+  authenticatingUser: 'Authenticating User',
+  authenticationFailed:
+    'Something went wrong when getting your information. Please try again',
+};
