a29216254a85ad64372f4ca57121d236ba6d2a26
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-11-20 12:33:47 +0100

    configured layout in routing developer app

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from '@mui/material';
 import { Auth } from '@ricly/auth';
 import { useIntl } from 'react-intl';
 import { Navigate } from 'react-router';
+import Layout from '../pages/Layout';
 
 export const Test = () => {
   const { formatMessage } = useIntl();
@@ -16,6 +17,11 @@ export const routes = [
   {
     path: '/',
     element: <Auth />,
+  },
+  {
+    path: '-',
+    element: <Layout />,
+    children: [{ path: 'schools', element: <Auth /> }],
   },
   {
     path: '*',
