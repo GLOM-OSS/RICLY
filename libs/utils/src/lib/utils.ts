398236b900e336e646fa2d4f2d72cb5a03975325
Marco Kuidja <kuidjamarco@gmail.com>
2022-11-20 14:01:25 +0100

    Created utils library and Added csv parser function to it

---
+++ libs/utils/src/lib/utils.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import { parse } from 'csv-parse';
+import { finished } from 'stream/promises';
+
+// Read and process the CSV file
+export async function processFile<T>(
+  columns: string[],
+  csvStream: fs.ReadStream
+) {
+  const records: T[] = [];
+  const parser = csvStream.pipe(
+    parse({
+      cast: true,
+      skip_empty_lines: false,
+      columns: columns.map((column) => ({ name: column })),
+    })
+  );
+  parser.on('readable', function () {
+    let record: T;
+    while ((record = parser.read()) !== null) {
+      // Work with each record
+      records.push(record);
+    }
+  });
+  await finished(parser);
+  return records;
+}
