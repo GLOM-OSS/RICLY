3d6958c09b10883a09e5c09be41e806f5f93a14b
Marco Kuidja <kuidjamarco@gmail.com>
2022-11-20 14:41:12 +0100

    Changed api utils folder position

--- apps/api/src/app/school/school.service.ts
+++ apps/api/src/app/school/school.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { getRandomString } from '../../prisma/utils';
+import { getRandomString } from '../../utils';
 import { SchoolPostDto } from '../app.dto';
 
 @Injectable()
