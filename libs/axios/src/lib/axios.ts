a50c5cb0ce6941da38884f5406d0997dfe607d21
Marco Kuidja <kuidjamarco@gmail.com>
2022-11-22 01:32:17 +0100

    Added axois configuration file as library to the project

---
+++ libs/axios/src/lib/axios.ts
@@ -0,0 +1,54 @@
+import { decrypt, encrypt } from '@ricly/encrypter';
+import axios, { AxiosInstance } from 'axios';
+
+export async function getCurrentIp() {
+  const { data: ip_address } = await axios.get('https://api.ipify.org');
+  return ip_address as string;
+}
+
+function axiosInstance(): AxiosInstance {
+  const app_lang = 'ricly_lang';
+
+  const axiosInstance = axios.create({
+    baseURL: process.env['NX_API_BASE_URL'],
+    headers: {
+      'Content-type': 'application/json',
+      'RICLY-API-KEY': process.env['NX_APP_API_KEY']
+    },
+    withCredentials: true,
+  });
+  axiosInstance.interceptors.request.use(
+    (request) => {
+      request = {
+        ...request,
+        headers: {
+          ...request.headers,
+          lang: localStorage.getItem(app_lang) || 'Fr',
+        },
+        params: request.params ? { data: encrypt(request.params) } : undefined,
+        data: request.data ? { data: encrypt(request.data) } : undefined,
+      };
+      return request;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      response = {
+        ...response,
+        data: decrypt(response.data),
+      };
+      return response;
+    },
+    (error) => {
+      if (error.response.statusCode === 403 && location.pathname !== '/signin')
+        location.href = '/signin';
+      return Promise.reject(error.response.data);
+    }
+  );
+
+  return axiosInstance;
+}
+
+export const http = axiosInstance();
